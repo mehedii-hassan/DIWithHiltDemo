09eaf42c7c9210a9f190cf86b5659ed9e2d152ee
Mehedi Hassan <mehedii.hassan01@gmail.com>
2023-01-12 01:07:07 +0600

    finished

--- app/src/main/java/com/example/diwithhiltdemo/MainActivity.kt
+++ app/src/main/java/com/example/diwithhiltdemo/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.diwithhiltdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.diwithhiltdemo.adapters.CryptocurrencyAdapter
+import com.example.diwithhiltdemo.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var cryptocurrencyList: RecyclerView
+
+    // viewModels() delegate used to get
+    // by view models will automatically construct the viewmodels using Hilt
+    private val viewModel:MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cryptocurrencyList = findViewById<RecyclerView>(R.id.cryptocurrency_list)
+        cryptocurrencyList.layoutManager = LinearLayoutManager(this)
+
+        observeCryptoCurrency()
+    }
+
+    // Observing the live data
+    private fun observeCryptoCurrency() {
+        viewModel.cryptoCurrency.observe(this) {
+            cryptocurrencyList.adapter = CryptocurrencyAdapter(it)
+        }
     }
 }
---
+++ app/src/main/java/com/example/diwithhiltdemo/viewmodels/MainViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.diwithhiltdemo.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.diwithhiltdemo.models.Cryptocurrency
+import com.example.diwithhiltdemo.repo.CryptocurrencyRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+// @HiltViewModel will make models to be
+// created using Hilt's model factory
+// @Inject annotation used to inject all
+// dependencies to view model class
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val cryptocurrencyRepository: CryptocurrencyRepository
+) : ViewModel() {
+    private val cryptocurrencyEmitter = MutableLiveData<List<Cryptocurrency>>()
+    val cryptoCurrency: LiveData<List<Cryptocurrency>> = cryptocurrencyEmitter
+
+    init {
+        loadCryptocurrency()
+    }
+
+    // getting cryptocurrencies list using
+    // repository and passing it into live data
+    private fun loadCryptocurrency() {
+        cryptocurrencyEmitter.value = cryptocurrencyRepository.getCryptoCurrency()
+    }
+}
